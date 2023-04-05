bc966402d48747a614a40b17d4ebbd9d18b48c05
Vincent Akuma@DESKTOP-1160AV7 MINGW64 ~ <akumavincent140@gmail.com>
2023-04-05 01:32:38 +0100

    ViewModel Commit

--- app/src/main/java/com/example/mvvmtutorial/MainActivity.kt
+++ app/src/main/java/com/example/mvvmtutorial/MainActivity.kt
@@ -2,15 +2,43 @@ package com.example.mvvmtutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
+import com.example.mvvmtutorial.Adapter.NotesAdapter
+import com.example.mvvmtutorial.database.NoteDataBase
 import com.example.mvvmtutorial.databinding.ActivityMainBinding
+import com.example.mvvmtutorial.models.Note
+import com.example.mvvmtutorial.models.NoteViewModel
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var dataBase : NoteDataBase
+    lateinit var viewModel: NoteViewModel
+    lateinit var adapter: NotesAdapter
+    lateinit var selectedNote: Note
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //Initializing the UI
+        initUI()
+
+        viewModel = ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory.getInstance(application))
+            .get(NoteViewModel::class.java)
+
+        viewModel.allNotes.observe(this) { list ->
+
+            list?.let {
+                adapter.updateList(list)
+            }
+        }
+    }
+
+    private fun initUI() {
+        TODO("Not yet implemented")
     }
 }
